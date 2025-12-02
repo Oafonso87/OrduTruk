d60f545f224aa515ed5c1a7eccf86be6076a80c4
Ales Martin <almartin@birt.eus>
2025-12-03 00:47:46 +0100

    feat: Implement UI for access and registration pages, including new styling, component imports, and an image asset.

--- src/app/pages/acceso/acceso.ts
+++ src/app/pages/acceso/acceso.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Button } from '../../components/button/button';
+import { Header } from '../../components/header/header';
+import { Footer } from '../../components/footer/footer';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-acceso',
-  standalone: false,
+  standalone: true,
+  imports: [Button, Header, Footer, RouterLink],
   templateUrl: './acceso.html',
   styleUrl: './acceso.css',
 })
