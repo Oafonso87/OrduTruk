d04b2b7de879cf32c3d2f47336be65d5b297db12
Ales Martin <almartin@birt.eus>
2025-12-10 15:31:34 +0100

    feat: add messages, create offers, and profile pages with their respective routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,15 +8,21 @@ import { QuienesSomos } from './pages/quienes-somos/quienes-somos';
 import { Registro } from './pages/registro/registro';
 import { Acceso } from './pages/acceso/acceso';
 import { OfertasDetalle } from './pages/ofertas-detalle/ofertas-detalle';
+import { CrearOfertas } from './pages/crear-ofertas/crear-ofertas';
+import { Perfil } from './pages/perfil/perfil';
+import { Mensajes } from './pages/mensajes/mensajes';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'ofertas', component: OfertasComponent },
     { path: 'ofertasdetalle', component: OfertasDetalle },
+    { path: 'crearoferta', component: CrearOfertas },
     { path: 'demandas', component: DemandasComponent },
     { path: 'usuarios', component: UsuariosComponent },
     { path: 'quienes-somos', component: QuienesSomos },
     { path: 'registro', component: Registro },
     { path: 'acceso', component: Acceso },
+    { path: 'perfil', component: Perfil },
+    { path: 'mensajes', component: Mensajes },
     { path: '**', component: HomeComponent }
 ];
---
+++ src/app/pages/mensajes/mensajes.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Header } from '../../components/header/header';
+import { Footer } from '../../components/footer/footer';
+import { Button } from '../../components/button/button';
+
+@Component({
+    selector: 'app-mensajes',
+    standalone: true,
+    imports: [CommonModule, Header, Footer, Button],
+    templateUrl: './mensajes.html',
+    styleUrl: './mensajes.scss',
+})
+export class Mensajes implements OnInit {
+    ngOnInit(): void {
+    }
+
+    constructor() { }
+
+    public vistaActual: 'listado' | 'detalle' | 'aniadir' = 'listado';
+    public mostrarFiltro: boolean = false;
+
+    // Array para repetir mensajes 6 veces
+    public mensajes = Array(6).fill(0);
+
+    toggleFiltro(): void {
+        this.mostrarFiltro = !this.mostrarFiltro;
+    }
+}
