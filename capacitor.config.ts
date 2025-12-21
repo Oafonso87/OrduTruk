9c1b73e0d2488b21bdb6775cf6cb3c043cf19689
Ales Martin <almartin@birt.eus>
2025-12-21 16:04:50 +0100

    feat: Initialize Capacitor project for Android and iOS platforms with default assets and configurations.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ordutruk.app',
+  appName: 'OrduTruk',
+  webDir: 'dist/ordutruk/browser'
+};
+
+export default config;
